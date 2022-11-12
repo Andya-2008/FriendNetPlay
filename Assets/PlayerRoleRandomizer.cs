214a9e6de080b5172d70c46873ed3816b2a0267d
Andya08 <andrewhyun@live.com>
2022-11-11 19:01:05 -0800

    Networking problems

--- Assets/PlayerRoleRandomizer.cs
+++ Assets/PlayerRoleRandomizer.cs
@@ -5,45 +5,87 @@ using Photon.Pun;
 
 public class PlayerRoleRandomizer : MonoBehaviour
 {
-    [SerializeField] Canvas MoverCanvas;
-    [SerializeField] Canvas PivotCanvas1;
-    [SerializeField] Canvas PivotCanvas2;
-    [SerializeField] Canvas ClawCanvas;
+    [SerializeField] Canvas MoverCanvas4;
+    [SerializeField] Canvas PivotCanvas14;
+    [SerializeField] Canvas PivotCanvas24;
+    [SerializeField] Canvas ClawCanvas4;
+    [SerializeField] Canvas MoverCanvas3;
+    [SerializeField] Canvas PivotCanvas13;
+    [SerializeField] Canvas PivotCanvas23;
+    [SerializeField] Canvas MoverCanvas2;
+    [SerializeField] Canvas PivotCanvas12;
 
     // Start is called before the first frame update
     void Start()
     {
         if (PhotonNetwork.IsConnectedAndReady)
         {
-            if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[1].NickName)
+            if (PhotonNetwork.PlayerList.Length == 5)
             {
-                MoverCanvas.GetComponent<Canvas>().enabled = true;
-                PivotCanvas1.GetComponent<Canvas>().enabled = false;
-                PivotCanvas2.GetComponent<Canvas>().enabled = false;
-                ClawCanvas.GetComponent<Canvas>().enabled = false;
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[1].NickName)
+                {
+                    MoverCanvas4.GetComponent<Canvas>().enabled = true;
+                    PivotCanvas14.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas24.GetComponent<Canvas>().enabled = false;
+                    ClawCanvas4.GetComponent<Canvas>().enabled = false;
+                }
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[2].NickName)
+                {
+                    MoverCanvas4.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas14.GetComponent<Canvas>().enabled = true;
+                    PivotCanvas24.GetComponent<Canvas>().enabled = false;
+                    ClawCanvas4.GetComponent<Canvas>().enabled = false;
+                }
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[3].NickName)
+                {
+                    MoverCanvas4.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas14.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas24.GetComponent<Canvas>().enabled = true;
+                    ClawCanvas4.GetComponent<Canvas>().enabled = false;
+                }
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[4].NickName)
+                {
+                    MoverCanvas4.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas14.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas24.GetComponent<Canvas>().enabled = false;
+                    ClawCanvas4.GetComponent<Canvas>().enabled = true;
+                }
             }
-            if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[2].NickName)
+            if (PhotonNetwork.PlayerList.Length == 4)
             {
-                MoverCanvas.GetComponent<Canvas>().enabled = false;
-                PivotCanvas1.GetComponent<Canvas>().enabled = true;
-                PivotCanvas2.GetComponent<Canvas>().enabled = false;
-                ClawCanvas.GetComponent<Canvas>().enabled = false;
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[1].NickName)
+                {
+                    MoverCanvas3.GetComponent<Canvas>().enabled = true;
+                    PivotCanvas13.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas23.GetComponent<Canvas>().enabled = false;
+                }
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[2].NickName)
+                {
+                    MoverCanvas3.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas13.GetComponent<Canvas>().enabled = true;
+                    PivotCanvas23.GetComponent<Canvas>().enabled = false;
+                }
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[3].NickName)
+                {
+                    MoverCanvas3.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas13.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas23.GetComponent<Canvas>().enabled = true;
+                }
             }
-            if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[3].NickName)
+            if (PhotonNetwork.PlayerList.Length == 3)
             {
-                MoverCanvas.GetComponent<Canvas>().enabled = false;
-                PivotCanvas1.GetComponent<Canvas>().enabled = false;
-                PivotCanvas2.GetComponent<Canvas>().enabled = true;
-                ClawCanvas.GetComponent<Canvas>().enabled = false;
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[1].NickName)
+                {
+                    MoverCanvas2.GetComponent<Canvas>().enabled = true;
+                    PivotCanvas12.GetComponent<Canvas>().enabled = false;
+                }
+                if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[2].NickName)
+                {
+                    MoverCanvas2.GetComponent<Canvas>().enabled = false;
+                    PivotCanvas12.GetComponent<Canvas>().enabled = true;
+                }
             }
-            if (PhotonNetwork.LocalPlayer.NickName == PhotonNetwork.PlayerList[4].NickName)
-            {
-                MoverCanvas.GetComponent<Canvas>().enabled = false;
-                PivotCanvas1.GetComponent<Canvas>().enabled = false;
-                PivotCanvas2.GetComponent<Canvas>().enabled = false;
-                ClawCanvas.GetComponent<Canvas>().enabled = true;
             }
-        }
     }
 
     // Update is called once per frame
